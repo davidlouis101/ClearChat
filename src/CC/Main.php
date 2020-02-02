3a81ab78a5e0f982c6999a06847668d76986db79
davidlouis101 <54008552+davidlouis101@users.noreply.github.com>
2020-02-02 10:01:05 +0100

    Send you extra message

--- src/CC/Main.php
+++ src/CC/Main.php
@@ -128,11 +128,13 @@ class Main extends PluginBase implements Listener{
 
 			$this->getServer()->broadcastMessage(""); $this->getServer()->broadcastMessage("Löscht Chat..."); $this->getServer()->broadcastMessage("Löscht Chat...");
 
-			$this->getServer()->broadcastMessage("§aChat wurde Gelöscht§b Vob §c$name");
+			$this->getServer()->broadcastMessage("§aChat wurde Gelöscht.§b Von §c$name");
 
                         $this->getServer()->broadcastMessage("§4Programmiert Von §9Crow§c Balde");
 
                         $this->getServer()->broadcastMessage("§bDiscord : https://discord.gg/rrf3gqh");
+
+                        $sender->sendMessage("("§4Hast Denn Chat Gelöscht(YT: Crow Balde)");
 
         }
 
